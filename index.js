b655223c3e64dd44320e7099a70d0c3ffaa02f6c
Sebastian Rodriguez <sebas1231998@gmail.com>
2021-10-28 00:31:47 -0500

    reajuste

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/api/proyectos', require('./routes/proyectos'))
 app.use('/api/tareas', require('./routes/tareas'))
 
 
-app.listen( port , 0.0.0.0,  ()=>{
+app.listen( port , '0.0.0.0' ,  ()=>{
 
  console.log("servidor funcionando: " + PORT)
 });
